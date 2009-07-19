742a0d4d7f9f9107c43f3dde36e713945f471e98
Niran Babalola <niran@niran.org>
2009-07-19 02:47:02 -0500

    Add license and description

--- merge_csv_contacts.py
+++ merge_csv_contacts.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python
+#
+# merge_csv_contacts.py
+#
+# Given a CSV file in Google's contacts format, merge the rows with the same name.
 # Usage: ./merge_csv_contacts.py <source> <output filename>
+#
+# Copyright 2009 Niran Babalola
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import sys
 import re
